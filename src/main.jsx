690ff9ebb2367572cf5512947ddcde6237bf1bb2
ranveer637 <craftdeindia3@gmail.com>
2025-11-13 00:37:27 +0530

    Create main.jsx

---
+++ src/main.jsx
@@ -0,0 +1,15 @@
+// src/main.jsx
+// Vite + React main entry point
+
+
+import React from 'react'
+import ReactDOM from 'react-dom/client'
+import './index.css'
+import AIResumeAnalyzer from './App'
+
+
+ReactDOM.createRoot(document.getElementById('root')).render(
+<React.StrictMode>
+<AIResumeAnalyzer />
+</React.StrictMode>
+)
