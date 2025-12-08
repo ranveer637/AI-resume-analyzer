42fdc95be4d5f4b1176f312dba7a37da29dda0e1
ranveer637 <craftdeindia3@gmail.com>
2025-12-08 14:41:35 +0530

    Update main.jsx

--- src/main.jsx
+++ src/main.jsx
@@ -1,15 +1,35 @@
 // src/main.jsx
-// Vite + React main entry point
+import React from "react";
+import ReactDOM from "react-dom/client";
+import { BrowserRouter, Routes, Route, Navigate } from "react-router-dom";
 
+import "./index.css";
+import App from "./App";
+import Login from "./Login";
+import Register from "./Register";
 
-import React from 'react'
-import ReactDOM from 'react-dom/client'
-import './index.css'
-import AIResumeAnalyzer from './App'
+function Root() {
+  return (
+    <BrowserRouter>
+      <Routes>
+        {/* Main resume analyzer */}
+        <Route path="/" element={<App />} />
+        {/* Optional alias route */}
+        <Route path="/analyzer" element={<App />} />
 
+        {/* Auth pages */}
+        <Route path="/login" element={<Login />} />
+        <Route path="/register" element={<Register />} />
 
-ReactDOM.createRoot(document.getElementById('root')).render(
-<React.StrictMode>
-<AIResumeAnalyzer />
-</React.StrictMode>
-)
+        {/* Fallback: any unknown route → home */}
+        <Route path="*" element={<Navigate to="/" replace />} />
+      </Routes>
+    </BrowserRouter>
+  );
+}
+
+ReactDOM.createRoot(document.getElementById("root")).render(
+  <React.StrictMode>
+    <Root />
+  </React.StrictMode>
+);
